76bb1e60c854f8182b4a6a810a4632631e87308f
spendyala <pvvssubrahmanyam@gmail.com>
2015-09-12 14:39:27 -0700

    Tab to space

--- static_html/urls.py
+++ static_html/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from static_html import views
 
 urlpatterns = [
-	# /static_html/
-	url(r'^((?P<file_name>[a-zA-Z0-9-]*)/|$)', views.index, name='index'),
+   # /static_html
+   url(r'^((?P<file_name>[a-zA-Z0-9-]*)/|$)', views.index, name='index'),
 ]
