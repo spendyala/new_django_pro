d5fd21dd723e42421d7b751bc6c5087946ecd8c0
Veera Pendyala <veera.pendyala@vpendyala-mbp.fireeye.com>
2015-09-12 11:55:22 -0700

    Clients model

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client  # , Comments
+
+class ClientAdmin(admin.ModelAdmin):
+	# fields = ['client_name', 'country', 'start_date']
+	list_filter = ['start_date', 'country']
+	list_display = ('client_name', 'country', 'start_date', 'was_recent')
+	fieldsets = [
+		(None,               {'fields': ['client_name']}),
+		('More information', {'fields': ['country', 'customer_site', 'start_date'],
+							  'classes': ['collapse']}),
+	]
+
+admin.site.register(Client, ClientAdmin)
+# admin.site.register(Comments)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+import datetime
+import ISO3166
 
-# Create your models here.
+
+class Client(models.Model):
+	client_name = models.CharField('Client', max_length=256)
+	country = models.CharField(max_length=2, choices=ISO3166.ISO3166)
+	start_date = models.DateTimeField('Registered Date')
+	customer_site = models.CharField('Customer Site', max_length=256, default='')
+
+	def was_recent(self):
+		return self.start_date >= timezone.now() - datetime.timedelta(days=1)
+	was_recent.admin_order_field = 'start_date'
+	was_recent.boolean = True
+	was_recent.short_description = 'Recently Joined?'
+
+	def __str__(self):
+		return self.client_name
+
+# class Comments(models.Model):
+# 	comment = models.TextField('Comments', max_length=500)
