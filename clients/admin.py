984fbdd67ffb0d7f109386482f429ade11986d26
Veera Pendyala <veera.pendyala@vpendyala-mbp.fireeye.com>
2015-09-12 13:09:55 -0700

    Client Framework admin, urls

--- clients/admin.py
+++ clients/admin.py
@@ -2,17 +2,14 @@ from django.contrib import admin
 
 from .models import Client  # , Comments
 
+
 class ClientAdmin(admin.ModelAdmin):
-	# fields = ['client_name', 'country', 'start_date']
-	list_filter = ['start_date', 'country']
-	# list_display = ('client_name', 'country', 'start_date', 'was_recent')
+	
 	list_display = ('client_name', 'country', 'was_recent')
-	fieldsets = [
-		(None,               {'fields': ['client_name']}),
-		# ('More information', {'fields': ['country', 'customer_site', 'start_date'],
-		('More information', {'fields': ['country', 'customer_site'],
-							  'classes': ['collapse']}),
-	]
+	list_filter = ['start_date', 'country']
+	fieldsets = [(None, {'fields': ['client_name']}),
+				 ('More information', {'fields': ['country', 'customer_site'],
+									   'classes': ['collapse']})]
 
 admin.site.register(Client, ClientAdmin)
 # admin.site.register(Comments)
--- clients/urls.py
+++ clients/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url, include
-from rest_framework.urlpatterns import format_suffix_patterns
-# from snippets import views
-from rest_framework import renderers
-# from snippets.views import ClientViewSet, UserViewSet
-# from django.conf.urls import url, include
 from clients import views
+
+from django.conf.urls import url, include
+
+from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
@@ -14,6 +13,6 @@ router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
 		url(r'^', include(router.urls)),
-		url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-		url(r'^html_client', )
+		url(r'^api-auth/', include('rest_framework.urls',
+								   namespace='rest_framework'))
 ]
