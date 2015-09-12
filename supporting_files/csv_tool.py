9e0ac500d079df67d0d4495cc9d022f1ccb6f373
Veera Pendyala <veera.pendyala@vpendyala-mbp.fireeye.com>
2015-09-12 11:07:38 -0700

    Steam Trap CSV

---
+++ supporting_files/csv_tool.py
@@ -0,0 +1,5 @@
+import csv
+with open('steam_trap.csv', 'rbU') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        print ','.join(row)
