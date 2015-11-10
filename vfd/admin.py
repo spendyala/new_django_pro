146e8fce0ef409a10cc577f420e9d0e22b3500ea
Veera Pendyala <veera.pendyala@vpendyala-mbp.fireeye.com>
2015-11-10 15:04:57 -0800

    VFD Updates PEP-8

--- vfd/admin.py
+++ vfd/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import LaborVFDMotor, MaterialsVFDMotor, VfdMotorSetpointSelections, VfdMotorDataPerMonth, VfdMotor
+from .models import (LaborVFDMotor,
+                     MaterialsVFDMotor,
+                     VfdMotorSetpointSelections,
+                     VfdMotorDataPerMonth,
+                     VfdMotor)
 admin.site.register(VfdMotor)
 admin.site.register(LaborVFDMotor)
 admin.site.register(MaterialsVFDMotor)
--- vfd/views.py
+++ vfd/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from vfd.models import LaborVFDMotor, MaterialsVFDMotor, VfdMotorSetpointSelections, VfdMotorDataPerMonth, VfdMotor
-from vfd.serializers import LaborVFDMotorSerializer, MaterialsVFDMotorSerializer, VfdMotorSetpointSelectionsSerializer, VfdMotorDataPerMonthSerializer, VfdMotorSerializer
+from vfd.models import (LaborVFDMotor,
+                        MaterialsVFDMotor,
+                        VfdMotorSetpointSelections,
+                        VfdMotorDataPerMonth,
+                        VfdMotor)
+from vfd.serializers import (LaborVFDMotorSerializer,
+                             MaterialsVFDMotorSerializer,
+                             VfdMotorSetpointSelectionsSerializer,
+                             VfdMotorDataPerMonthSerializer,
+                             VfdMotorSerializer)
 # from clients.serializers import UserSerializer
 from django.contrib.auth.models import User
 
@@ -15,110 +23,110 @@ import django_filters
 
 
 class VfdMotorFilter(django_filters.FilterSet):
-	class Meta:
-		model = VfdMotor
-		fields = ['client']
+    class Meta:
+        model = VfdMotor
+        fields = ['client']
 
 # Create your views here.
 class VfdMotorViewSet(viewsets.ModelViewSet):
-		"""
-		This viewset automatically provides `list`, `create`, `retrieve`,
-		`update` and `destroy` actions.
-		"""
-		filter_backends = (filters.DjangoFilterBackend,)
-		filter_class = VfdMotorFilter
-		queryset = VfdMotor.objects.all()
-		serializer_class = VfdMotorSerializer
-		permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-												  IsOwnerOrReadOnly)
+        """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+        """
+        filter_backends = (filters.DjangoFilterBackend,)
+        filter_class = VfdMotorFilter
+        queryset = VfdMotor.objects.all()
+        serializer_class = VfdMotorSerializer
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                              IsOwnerOrReadOnly)
 
-		def perform_create(self, serializer):
-			serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
 class VfdMotorDataPerMonthFilter(django_filters.FilterSet):
-	class Meta:
-		model = VfdMotorDataPerMonth
-		fields = ['client_vfd']
+    class Meta:
+        model = VfdMotorDataPerMonth
+        fields = ['client_vfd']
 
 # Create your views here.
 class VfdMotorDataPerMonthViewSet(viewsets.ModelViewSet):
-		"""
-		This viewset automatically provides `list`, `create`, `retrieve`,
-		`update` and `destroy` actions.
-		"""
-		filter_backends = (filters.DjangoFilterBackend,)
-		filter_class = VfdMotorDataPerMonthFilter
-		queryset = VfdMotorDataPerMonth.objects.all()
-		serializer_class = VfdMotorDataPerMonthSerializer
-		permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-												  IsOwnerOrReadOnly)
+        """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+        """
+        filter_backends = (filters.DjangoFilterBackend,)
+        filter_class = VfdMotorDataPerMonthFilter
+        queryset = VfdMotorDataPerMonth.objects.all()
+        serializer_class = VfdMotorDataPerMonthSerializer
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                              IsOwnerOrReadOnly)
 
-		def perform_create(self, serializer):
-			serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
 class VfdMotorSetpointSelectionsFilter(django_filters.FilterSet):
-	class Meta:
-		model = VfdMotorSetpointSelections
-		fields = ['client_vfd']
+    class Meta:
+        model = VfdMotorSetpointSelections
+        fields = ['client_vfd']
 
 # Create your views here.
 class VfdMotorSetpointSelectionsViewSet(viewsets.ModelViewSet):
-		"""
-		This viewset automatically provides `list`, `create`, `retrieve`,
-		`update` and `destroy` actions.
-		"""
-		filter_backends = (filters.DjangoFilterBackend,)
-		filter_class = VfdMotorSetpointSelectionsFilter
-		queryset = VfdMotorSetpointSelections.objects.all()
-		serializer_class = VfdMotorSetpointSelectionsSerializer
-		permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-												  IsOwnerOrReadOnly)
+        """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+        """
+        filter_backends = (filters.DjangoFilterBackend,)
+        filter_class = VfdMotorSetpointSelectionsFilter
+        queryset = VfdMotorSetpointSelections.objects.all()
+        serializer_class = VfdMotorSetpointSelectionsSerializer
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                              IsOwnerOrReadOnly)
 
-		def perform_create(self, serializer):
-			serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
 class MaterialsVFDMotorFilter(django_filters.FilterSet):
-	class Meta:
-		model = MaterialsVFDMotor
-		fields = ['client_vfd']
+    class Meta:
+        model = MaterialsVFDMotor
+        fields = ['client_vfd']
 
 # Create your views here.
 class MaterialsVFDMotorViewSet(viewsets.ModelViewSet):
-		"""
-		This viewset automatically provides `list`, `create`, `retrieve`,
-		`update` and `destroy` actions.
-		"""
-		filter_backends = (filters.DjangoFilterBackend,)
-		filter_class = MaterialsVFDMotorFilter
-		queryset = MaterialsVFDMotor.objects.all()
-		serializer_class = MaterialsVFDMotorSerializer
-		permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-												  IsOwnerOrReadOnly)
+        """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+        """
+        filter_backends = (filters.DjangoFilterBackend,)
+        filter_class = MaterialsVFDMotorFilter
+        queryset = MaterialsVFDMotor.objects.all()
+        serializer_class = MaterialsVFDMotorSerializer
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                                                  IsOwnerOrReadOnly)
 
-		def perform_create(self, serializer):
-			serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
 
 
 class LaborVFDMotorFilter(django_filters.FilterSet):
-	class Meta:
-		model = LaborVFDMotor
-		fields = ['client_vfd']
+    class Meta:
+        model = LaborVFDMotor
+        fields = ['client_vfd']
 
 # Create your views here.
 class LaborVFDMotorViewSet(viewsets.ModelViewSet):
-		"""
-		This viewset automatically provides `list`, `create`, `retrieve`,
-		`update` and `destroy` actions.
-		"""
-		filter_backends = (filters.DjangoFilterBackend,)
-		filter_class = LaborVFDMotorFilter
-		queryset = LaborVFDMotor.objects.all()
-		serializer_class = LaborVFDMotorSerializer
-		permission_classes = (permissions.IsAuthenticatedOrReadOnly,
-												  IsOwnerOrReadOnly)
+        """
+        This viewset automatically provides `list`, `create`, `retrieve`,
+        `update` and `destroy` actions.
+        """
+        filter_backends = (filters.DjangoFilterBackend,)
+        filter_class = LaborVFDMotorFilter
+        queryset = LaborVFDMotor.objects.all()
+        serializer_class = LaborVFDMotorSerializer
+        permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                                                  IsOwnerOrReadOnly)
 
-		def perform_create(self, serializer):
-			serializer.save(owner=self.request.user)
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
